4b02679c14d71920f26e722d9af9487984b0ad62
Parth <parthshukla285@gmail.com>
2022-07-27 17:44:11 +0530

    change dockerfile

--- EmileMaleV1/run.sh
+++ EmileMaleV1/run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-source venv/bin/activate
 python3 stage1.py
 echo stage 1 done
 
 cd YOLOv6
-python tools/infer.py --yaml data/dataset.yaml --img-size 416 --weights runs/train/exp1/weights/best_ckpt.pt --source ../data/images
+python3 tools/infer.py --yaml data/dataset.yaml --img-size 416 --weights runs/train/exp1/weights/best_ckpt.pt --source ../data/images
 mkdir ../out2
 cp -r runs/inference/exp/ ../out2/
 cd ..
